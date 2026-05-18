043ca43e0fc236c52fb89fb73e621644ce04986d
Travis Cline <travis.cline@gmail.com>
2026-05-17 22:59:02 -0700

    test: cover bundle descriptor restore

--- test/html/run-bundle-harness.mjs
+++ test/html/run-bundle-harness.mjs
@@ -16,6 +16,7 @@ const defaultPages = [
 	"test/html/bundle-bindgraph-restore.html",
 	"test/html/bundle-cowfs-restore.html",
 	"test/html/bundle-running-task-restore.html",
+	"test/html/bundle-fd-restore.html",
 	"test/html/bundle-service-worker-restore.html",
 	"test/html/bundle-composite-restore.html",
 	"test/html/bundle-v86-state-restore.html",
