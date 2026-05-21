14c7cb377516e1c90dba07a60aac3bcf1e38d4ba
Travis Cline <travis.cline@gmail.com>
2026-05-21 11:48:28 -0700

    api: accept integer chtimes arguments

--- api/chtimes.go
+++ api/chtimes.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"math"
 	"time"
 
 	"tractor.dev/toolkit-go/duplex/rpc"
@@ -13,25 +14,60 @@ func (s *syscaller) chtimes(r rpc.Responder, c *rpc.Call) {
 
 	path, ok := args[0].(string)
 	if !ok {
-		panic("arg 0 is not a string")
+		r.Return(errInvalidArg("chtimes", "path"))
+		return
 	}
 
 	// atime and mtime are in seconds (with fractional parts)
-	atimeSec, ok := args[1].(float64)
-	if !ok {
-		panic("arg 1 is not a float64")
-	}
-	atime := time.Unix(int64(atimeSec), int64((atimeSec-float64(int64(atimeSec)))*1e9))
-
-	mtimeSec, ok := args[2].(float64)
-	if !ok {
-		panic("arg 2 is not a float64")
-	}
-	mtime := time.Unix(int64(mtimeSec), int64((mtimeSec-float64(int64(mtimeSec)))*1e9))
-
-	err := fs.Chtimes(s.task.NS(), path, atime, mtime)
+	atime, err := decodeUnixSeconds(args, 1)
 	if err != nil {
 		r.Return(err)
 		return
+	}
+	mtime, err := decodeUnixSeconds(args, 2)
+	if err != nil {
+		r.Return(err)
+		return
+	}
+
+	err = fs.Chtimes(s.task.NS(), path, atime, mtime)
+	if err != nil {
+		r.Return(err)
+		return
+	}
+}
+
+func decodeUnixSeconds(args []any, index int) (time.Time, error) {
+	if len(args) <= index {
+		return time.Time{}, errInvalidArg("chtimes", "time")
+	}
+	sec, err := numberArg(args[index])
+	if err != nil {
+		return time.Time{}, err
+	}
+	whole, frac := math.Modf(sec)
+	return time.Unix(int64(whole), int64(frac*1e9)), nil
+}
+
+func numberArg(v any) (float64, error) {
+	switch v := v.(type) {
+	case float64:
+		return v, nil
+	case float32:
+		return float64(v), nil
+	case int:
+		return float64(v), nil
+	case int64:
+		return float64(v), nil
+	case int32:
+		return float64(v), nil
+	case uint:
+		return float64(v), nil
+	case uint64:
+		return float64(v), nil
+	case uint32:
+		return float64(v), nil
+	default:
+		return 0, errInvalidArg("chtimes", "time")
 	}
 }
